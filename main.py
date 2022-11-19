0fafaa7ab9be77360da0030720a636f57631339e
mgeneja <m.geneja@stud.elka.pw.edu.pl>
2022-11-19 23:15:03 +0100

    Uploads data to given Google Sheet

--- main.py
+++ main.py
@@ -1,35 +1,48 @@
 """
 The Python script to get podcast data from given sites and organize it into .csv table
 """
+import xml.etree.ElementTree as ET
 from datetime import date
-from typing import Tuple
+from typing import Tuple, List
+from urllib.request import urlopen
 
-import scrapetube
-from pytube import YouTube
+import gspread
 
 
-def get_video_data(sources: dict, last_download: date) -> dict:
+def get_podcast_data(feed_url: str, last_download: date) -> List[dict]:
 	"""
-	Function to get video's data
+	Function to extract podcast data from RSS feed
 
-	:param sources: Dictionary of channels' urls
-	:param last_download: Date of last download
-	:return: Dictionary with the channel's name as a key, and list of videos (YouTube objects) as a value
+	:param feed_url: URL of the RSS feed
+	:param last_download: Date when the program run last time
+	:return: List of episodes of a podcast with given RSS feed since the last_download
 	"""
-	url_prefix = "https://www.youtube.com/watch?v="
-	videos = {k: [] for k in sources.keys()}
-	for ch, url in sources.items():
-		vid = scrapetube.get_channel(channel_url=url)
-		for v in vid:
-			video_url = url_prefix + v["videoId"]
-			video = YouTube(video_url)
-			publish_day = video.publish_date
-			y, m, d = tuple(str(publish_day).split()[0].split('-'))
-			publish_day = date(int(y), int(m), int(d))
-			if publish_day < last_download:
-				break
-			videos[ch].append(video)
-	return videos
+	episodes = []
+	root = ET.parse(urlopen(feed_url)).getroot()
+
+	for ep in root[0]:
+		if ep.tag != 'item':  # It means that we reached episode in XML tree
+			continue
+		episode = {}
+		for attr in ep:
+			attr.tag = attr.tag.split("{http://www.itunes.com/dtds/podcast-1.0.dtd}")[-1]
+			if attr.tag == "pubDate":
+				# noinspection PyUnresolvedReferences
+				split_date = attr.text.split()
+				# noinspection PyUnresolvedReferences
+				attr.text = date(int(split_date[3]), dates_map[split_date[2]], int(split_date[1]))
+			elif attr.tag == 'duration':
+				match len(attr.text.split(':')):
+					case 1:
+						attr.text = f'0:0:{attr.text}'
+					case 2:
+						attr.text = f'0:{attr.text}'
+
+			episode[attr.tag] = attr.text
+		if episode['pubDate'] < last_download:
+			break
+		episodes.append(episode)
+	return episodes
 
 
 def load_podcast_sources(podcast_sources: str):
@@ -58,24 +71,29 @@ def load_internal_data() -> Tuple[date]:
 			data = [line.strip().split() for line in data]
 			last_download = date(*[int(i) for i in data[0]])
 	except Exception:
-		last_download = date.today()
+		last_download = today
 	return last_download,
 
 
-def save_data(videos: dict) -> None:
+def save_data(podcasts: dict) -> None:
 	"""
 	Function to save data to .csv file
 
-	:param videos: Videos to be saved
+	:param podcasts: Podcasts to be saved
 	"""
-	today = date.today()
-	with open("output.csv", 'w', encoding='utf-8') as f:
-		for ch, vid in videos.items():
-			for v in vid:
-				y, m, d = tuple(str(v.publish_date).split()[0].split('-'))
-				upload_date = date(int(y), int(m), int(d))
-				title = v.title.split('|')[0]
-				f.write(f"{ch},{title},{upload_date},{today},0:0:{v.length},0\n")
+	gc = gspread.service_account(filename='credentials.json')
+	with open('output_worksheet_info.txt',encoding='utf-8')as f:
+		key = f.readline().strip().split(':')[1]
+		sheet = f.readline().strip().split(':')[1]
+	sh = gc.open_by_key(key)
+	worksheet = sh.worksheet(sheet)
+	to_save = []
+	for name, episodes in podcasts.items():
+		for ep in episodes:
+			title = ep['title'].split('|')[0]
+			to_save.append([name, title, str(ep['pubDate']), str(today), ep['duration'], '0'])
+	worksheet.append_rows(to_save, value_input_option='USER_ENTERED')
+
 	with open("internal_data.txt", 'w') as f:
 		f.write(f"{today.year} {today.month} {today.day}")
 
@@ -88,9 +106,23 @@ def main() -> None:
 
 	sources = load_podcast_sources(podcast_sources)
 	last_download, = load_internal_data()
-	videos = get_video_data(sources, last_download)
-	save_data(videos)
+	podcasts = {}
+
+	for name, feed_url in sources.items():
+		try:
+			podcasts[name] = get_podcast_data(feed_url, last_download)
+		except Exception:
+			print(f"There's a problem with {name} podcast, probably access forbidden")
+
+	save_data(podcasts)
 
 
 if __name__ == '__main__':
+	dates_map = {
+		'Jan': 1, 'Feb': 2, 'Mar': 3,
+		'Apr': 4, 'May': 5, 'Jun': 6,
+		'Jul': 7, 'Aug': 8, 'Sep': 9,
+		'Oct':10, 'Nov':11, 'Dec':12
+	}
+	today = date.today()
 	main()
